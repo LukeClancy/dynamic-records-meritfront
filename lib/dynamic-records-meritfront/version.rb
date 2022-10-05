38efe8863ce7a70b5e96e769625024bba0c127b1
Luke Clancy <lukeclancy@hotmail.com>
2022-10-05 11:40:25 -0700

    its finally working

--- lib/dynamic-records-meritfront/version.rb
+++ lib/dynamic-records-meritfront/version.rb
@@ -1,5 +1,5 @@
 
 	module DynamicRecordsMeritfront
-	    VERSION = '0.1.4'
+	    VERSION = '0.1.7'
 	end
 	#this file gets overwritten automatically on minor updates, major ones need to be manually changed
