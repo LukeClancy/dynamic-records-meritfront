e560fde526d3b950b8beaa938cac192891d48a20
Luke Clancy <lukeclancy@hotmail.com>
2022-10-05 11:46:24 -0700

    minor changes to version 1.1.8

--- lib/dynamic-records-meritfront/version.rb
+++ lib/dynamic-records-meritfront/version.rb
@@ -1,5 +1,5 @@
 
 	module DynamicRecordsMeritfront
-	    VERSION = '0.1.7'
+	    VERSION = '1.1.8'
 	end
 	#this file gets overwritten automatically on minor updates, major ones need to be manually changed
