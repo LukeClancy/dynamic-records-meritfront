5cd0aa87b97c037775af951acd952f37a2156d21
Luke Clancy <lukeclancy@hotmail.com>
2022-10-17 15:31:28 -0700

    small change to the blind_hgid method to allow one to not encode the hgid (thus making it a gid)

--- lib/dynamic-records-meritfront/version.rb
+++ lib/dynamic-records-meritfront/version.rb
@@ -1,5 +1,5 @@
 
 	module DynamicRecordsMeritfront
-	    VERSION = '1.1.10'
+	    VERSION = '1.1.11'
 	end
 	#this file gets overwritten automatically on minor updates, major ones need to be manually changed
