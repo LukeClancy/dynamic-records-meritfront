e9e8bd3b260bc20feb3cc40c06b4d3fcc0e69701
Luke Clancy <lukeclancy@hotmail.com>
2022-12-14 11:17:19 -0800

    minor changes to version 3.0.4

--- lib/dynamic-records-meritfront/version.rb
+++ lib/dynamic-records-meritfront/version.rb
@@ -1,5 +1,5 @@
 
 	module DynamicRecordsMeritfront
-	    VERSION = '3.0.3'
+	    VERSION = '3.0.4'
 	end
 	#this file gets overwritten automatically on minor updates, major ones need to be manually changed
