7ca0d91bf0eee57242331e1c102cf88a017a074c
Luke Clancy <lukeclancy@hotmail.com>
2023-02-13 12:03:10 -0800

    minor breaking change and usability increase for instaload. I will try to keep changes that are breaking out of minor versions from now on as the gem is generally more stable now.

--- lib/dynamic-records-meritfront/version.rb
+++ lib/dynamic-records-meritfront/version.rb
@@ -1,5 +1,5 @@
 
 	module DynamicRecordsMeritfront
-	    VERSION = '3.0.27'
+	    VERSION = '3.1.0'
 	end
 	#this file gets overwritten automatically on minor updates, major ones need to be manually changed
