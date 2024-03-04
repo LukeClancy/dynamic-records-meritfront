5e93c091b6bfe7883a52cd908635e9a97ca946e3
Luke Clancy <lukeclancy@hotmail.com>
2024-03-04 13:56:22 -0800

    minor changes to version 3.1.32

--- lib/dynamic-records-meritfront/version.rb
+++ lib/dynamic-records-meritfront/version.rb
@@ -1,5 +1,5 @@
 
 	module DynamicRecordsMeritfront
-	    VERSION = '3.1.31'
+	    VERSION = '3.1.32'
 	end
 	#this file gets overwritten automatically on minor updates, major ones need to be manually changed
